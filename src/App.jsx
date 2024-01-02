2bce987f41af846b2073efabef57cb57d72f658a
cristibadea777 <cristibadea777@gmail.com>
2024-01-02 20:13:10 +0200

    2 Ianuarie 2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import ReactPlayer from "react-player/youtube";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightLong, faDiagramNext, faPause, faPlay, faVolumeHigh, faVolumeMute, faVolumeOff } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
 
-  const [link, setLink] = useState('')
-  const [index, setIndex] = useState(0)
-  const [links, setLinks] = useState([])
-  const [play, setPlay] = useState(false)
+  const [input,    setInput]    = useState('')
+  const [link,     setLink]     = useState('')
+  const [index,    setIndex]    = useState(0)
+  const [links,    setLinks]    = useState([])
+  const [playing,  setPlaying]  = useState(false)
+  const [muted,    setMuted]    = useState(false)
 
-  const handleChangeLink = (url) => {
+  const handleLoad = () => {
+    console.log(playing)
     setIndex(0)
-    if(url.includes('list=')){
+    if(input.includes('list=')){
       setLinks(['https://www.youtube.com/watch?v=cirDXY3CkSk', 'https://www.youtube.com/watch?v=-RAp9NT2tZY'])
-      setLink(links[index])
+      setLink(links[0])
     }
-    else if(ReactPlayer.canPlay(url)) setLink(url)
-    else { setLink('') }
+    else setLink(input)
   }
+
+  useEffect(() => {
+    setLink(links[0])
+  }, [index])
 
   const handleEnded = () => {
     if(links.length > 0){
@@ -26,29 +34,43 @@ function App() {
     }
   }
 
-  const handlePlay = () => {
-    setPlay(true)
-  }
-
   return (
     <div style={{maxWidth: "100vw", maxHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "flex-start", flexDirection: "column"}}>
       <div style={{width: "100%", height: "10vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
         <label style={{marginLeft: "7%", marginRight: "7%"}} htmlFor="inputLink">Link</label>
-        <input style={{width: "100%", marginRight: "7%"}} onChange={(event)=>{handleChangeLink(event.target.value)}}></input>
+        <input style={{width: "100%", marginRight: "7%"}} onChange={(event)=>{setInput(event.target.value)}}></input>
+        <button onClick={handleLoad}>Load</button>
       </div>
       {link && (
         <div style={{height: "86vh  ", width: "100vw"}}>
             <div style={{width: "100%", height: "80%", marginTop: "1%"}}>
               <ReactPlayer
                 url={link}
-                playing={play}
+                playing={playing}
                 width={"100%"} 
                 height={"100%"}
-                onReady={handlePlay} 
                 onEnded={handleEnded}
+                muted={muted}
+                onPlay={()=>{setPlaying(true)}}
+                onPause={()=>{setPlaying(false)}}
               />
             </div>
             <div style={{width: "100%", height: "20%", backgroundColor: "yellow"}}>
+              <button onClick={() => {setPlaying(!playing)} }> 
+                {!playing ? (
+                  <FontAwesomeIcon icon={faPlay}></FontAwesomeIcon>
+                ) : (
+                  <FontAwesomeIcon icon={faPause}></FontAwesomeIcon>
+                )}
+              </button>
+              <button onClick={() => {setMuted(!muted)} }> 
+                {!muted ? (
+                  <FontAwesomeIcon icon={faVolumeMute}></FontAwesomeIcon>
+                ) : (
+                  <FontAwesomeIcon icon={faVolumeHigh}></FontAwesomeIcon>
+                )}
+              </button>
+              <button onClick={() => {setIndex(index + 1)}}><FontAwesomeIcon icon={faArrowRightLong}></FontAwesomeIcon></button>
             </div>
         </div>
       )}
